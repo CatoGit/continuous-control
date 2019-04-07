d2e3d1b57cd3dd527cc1698908ab6d23c5586e56
Christopher Otto <44781219+CatoGit@users.noreply.github.com>
2019-04-07 11:12:28 +0200

    chore: updating build tasks

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='continuouscontrol',
+    version='1.0',
+    packages=['continuouscontrol',],
+    long_description=open('README.md').read(),
+)
